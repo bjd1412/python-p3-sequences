fbc64f8a5587fd0da5397f6d6da8b028eab780d0
Brendan Dempsey <bjd1412@gmail.com>
2024-05-06 23:32:56 -0400

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    i = [0, 1, 1, 2]
+    while len(i) < length:
+        fibo = i[-1] + i[-2]
+        i.append(fibo)
+    print(i[:length])    
+
